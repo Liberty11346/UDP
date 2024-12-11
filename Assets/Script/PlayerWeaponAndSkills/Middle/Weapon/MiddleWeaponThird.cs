fb8c72a2760b153c329b0aef0020bc85370f313f
MOKOTANinSITAO <167013401+MOKOTANinSITAO@users.noreply.github.com>
2024-12-11 17:06:44 +0900

    Revert "빨간버튼"
    This reverts commit e43d431119a1405f924f5bb980cbd356957fc361.

--- Assets/Script/PlayerWeaponAndSkills/Middle/Weapon/MiddleWeaponThird.cs
+++ Assets/Script/PlayerWeaponAndSkills/Middle/Weapon/MiddleWeaponThird.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine.UIElements;
 using UnityEngine;
-using System;
-
 using System;
 
 
 public class MiddleWeaponThird : PlayerWeaponBasic
 {
-    private MiddleBullet2 trackedBullet;  // 추적할 MiddleBullet2
+    // Start is called before the first frame update
+
+    GameObject DarkMatter;
+    Vector3 DarkMatterPos;
+
 
     void Start()
     {
-      
+         // 씬에서 "MiddleBullet2"라는 이름을 가진 오브젝트를 찾음
+        DarkMatter = GameObject.Find("MiddleBullet2");
+
+        DarkMatterPos = DarkMatter.transform.position;
+        if(DarkMatterPos == null)
+        {
+            Debug.Log("null");
+        }
 
         weaponName = "빨간 버튼";
         weaponExplain = "다크 매터와의 연계스킬\n 다크 매터를 폭파하여 적에게 피해를 입힙니다.";
 
-        for (int i = 0; i < 4; i++)
+         for(int i = 0; i < 4; i++)
         {
             projectileDamage[i] = 50 + i * 15;
             projectileAmount[i] = 1;
             maxCoolTime[i] = 13;
         }
+       
 
         GetCameraTransform();
         currentLevel = 3;
@@ -38,37 +49,33 @@ public class MiddleWeaponThird : PlayerWeaponBasic
     }
     public override void Fire()
     {
-        // 이미 생성된 MiddleBullet2를 추적하여 폭발 시킴
-        if (trackedBullet != null)
+        
+        if(Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(TriggerExplosion());
+        CreateExplode();
         }
-        else
-        {
-            Debug.LogWarning("No MiddleBullet2 object to track.");
-        }
+
     }
 
-    private IEnumerator TriggerExplosion()
+    void CreateExplode()
     {
-        // 일정 시간 대기 후 폭발 트리거 (0.5초 대기)
-        yield return new WaitForSeconds(0.5f);
+         GameObject fireProjectile = Instantiate(projectile, DarkMatterPos, DarkMatter.transform.rotation);
+         fireProjectile.GetComponent<PlayerBulletBasic>().Clone(this ,currentLevel);
+         Debug.Log("CreateExplode가 실행");
 
-        // trackedBullet이 존재하면 폭발 발생
-        if (trackedBullet != null)
+        
+        if(DarkMatter != null)
         {
-            trackedBullet.MadeExplode(trackedBullet.transform.position);  // 해당 위치에서 폭발 발생
-            Debug.Log("폭발 발생");
+            
+            Destroy(DarkMatter.gameObject);
+            DarkMatter = null;
+            Debug.Log("다크 메터가 제거 됨");
+
         }
-        else
-        {
-            Debug.LogWarning("MiddleBullet2 has been destroyed before explosion.");
-        }
+        
+         
     }
 
-    // 외부에서 MiddleBullet2 객체를 설정할 메서드
-    public void SetTrackedBullet(MiddleBullet2 bullet)
-    {
-        trackedBullet = bullet;
-    }
+    
+   
 }
